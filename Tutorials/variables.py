b5be4437c8fbb5f6a1faa2c725797309c1ffb867
{JSejrskild} <{johannesejrskild.1@gmail.com}>
2025-04-01 15:40:47 +1100

    first push

---
+++ Tutorials/variables.py
@@ -0,0 +1,25 @@
+'''
+Following week 1:
+- Variables
+
+'''
+
+#Variables types - poorly done by me :)
+x = 5
+name = "Alice"
+is_valid = True
+
+
+
+x = [1, 2, 3, 4, 5]
+y = []
+
+for i in x:
+  y.append(i * 3)
+
+print(y)
+
+
+x = True
+while x:
+  x = False
